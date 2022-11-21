124f559c55ece14943a9500b80545fcb007df4ed
Adolphus Cox <acox0234@gmail.com>
2022-11-21 15:13:14 -0700

    Updates

---
+++ precache-manifest.10e2765df289d13d21b0d7082d3eb7d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f0fd83f14c4d1dfe0fa8eb50d1d136",
+    "url": "/SmartBrainApp/index.html"
+  },
+  {
+    "revision": "72455829f287283e4425",
+    "url": "/SmartBrainApp/static/css/2.9b72255a.chunk.css"
+  },
+  {
+    "revision": "2f76cbfb3647f82385ef",
+    "url": "/SmartBrainApp/static/css/main.70309b87.chunk.css"
+  },
+  {
+    "revision": "72455829f287283e4425",
+    "url": "/SmartBrainApp/static/js/2.12db812b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/SmartBrainApp/static/js/2.12db812b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f76cbfb3647f82385ef",
+    "url": "/SmartBrainApp/static/js/main.76fdcfdd.chunk.js"
+  },
+  {
+    "revision": "4ed5a46ad5f436122ff3",
+    "url": "/SmartBrainApp/static/js/runtime-main.ad0ba213.js"
+  }
+]);
